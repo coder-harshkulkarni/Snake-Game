9746335f6c361a42c544040f2b2a4a4d19d22b54
“Harsh <harshkulkarni78@gmail.com>
2023-02-18 16:42:39 +0530

    Add file handling in this project data.txt contain high-score of player and in scoreboard.py we just write new high-score in the data.txt file

--- scoreboard.py
+++ scoreboard.py
@@ -9,7 +9,8 @@ class ScoreBoard(Turtle):
     def __init__(self, top_position):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open('data.txt') as file:
+            self.high_score = int(file.read())
         self.color("White")
         self.hideturtle()
         self.penup()
@@ -23,6 +24,8 @@ class ScoreBoard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open('data.txt', mode='w') as file:
+                file.write(str(self.high_score))
         self.score = 0
         self.update_scoreboard()
 
